682dd2a7e164411f38a6422c9ab7fd7630074208
Jun-ya HASEBA <7pairs@gmail.com>
2016-04-26 19:41:40 +0900

    Add settings for isparta #1

--- test/test-dummy.js
+++
@@ -1,7 +0,0 @@
-const assert = require("assert");
-import dummy from "../src/dummy";
-describe("dummy", function () {
-    it("should return 2", function () {
-        assert.equal(1 + 1, 2);
-    });
-});
